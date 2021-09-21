399f417bb444ca11ecca5217c7e7b3cdad56c797
LeoCPeres <leonacsik@gmail.com>
2021-09-20 21:14:27 -0300

    Modal form with RadioBox buttons

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,70 @@
+import Modal from "react-modal";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import closeImg from "../../assets/fechar.svg";
+import incomeImg from "../../assets/entradas.svg";
+import outcomeImg from "../../assets/saidas.svg";
+import { useState } from "react";
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
